7e245232ff41ef2c0a938de138f63083371a1c18
Ian Whitney <ian@ianwhitney.com>
2011-11-30 14:31:37 -0600

    fixes to gravity conversions. Handling of sugars that aren't part of the mash

--- lib/brewers_point.rb
+++
@@ -1,7 +0,0 @@
-require 'delegate'
-
-class BrewersPoint < DelegateClass(Float)
-  def to_sg
-    SpecificGravity.new(self / 1000 + 1)
-  end
-end
--- lib/specific_gravity.rb
+++
@@ -1,7 +0,0 @@
-require 'delegate'
-
-class SpecificGravity < DelegateClass(Float)
-  def to_brix
-    261.33*(1-1/self)
-  end
-end
